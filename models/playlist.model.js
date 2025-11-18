5c5ca4b1a1e086a36e247f1fe0bb228ea3826c99
zndionisio-neu <243483747+zndionisio-neu@users.noreply.github.com>
2025-11-19 01:50:46 +0800

    feat: GET, POST, and DELETE methods feat: Not showing deleted documents in JSON

--- models/playlist.model.js
+++ models/playlist.model.js
@@ -17,12 +17,24 @@ const PlaylistSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
   },
 );
 
+PlaylistSchema.set("toJSON", {
+  transform: (_, ret) => {
+    if (ret.songs) ret.songs = ret.songs.filter((song) => !song.deleted);
+    delete ret.deleted;
+    return ret;
+  },
+});
 const Playlist = mongoose.model("Playlist", PlaylistSchema);
 
 module.exports = Playlist;
--- models/song.model.js
+++ models/song.model.js
@@ -11,8 +11,23 @@ const SongSchema = mongoose.Schema(
       type: String,
       require: true,
     },
+
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
   },
 );
+
+SongSchema.set("toJSON", {
+  transform: (_, ret) => {
+    if (ret.deleted) return ret;
+    delete ret.deleted;
+    return ret;
+  },
+});
+
+module.exports = SongSchema;
