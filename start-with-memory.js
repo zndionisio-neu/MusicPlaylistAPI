5efb3845f20496b4d104ad1fda552c4876e14638
Christin10n <christine.laput@neu.edu.ph>
2025-11-23 02:02:26 +0800

    chore(dev): add start-with-memory script and mongodb-memory-server

---
+++ start-with-memory.js
@@ -0,0 +1,14 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+(async () => {
+  try {
+    const mongod = await MongoMemoryServer.create();
+    const uri = mongod.getUri();
+    process.env.MONGODB_URI = uri;
+    console.log('Using in-memory MongoDB at', uri);
+    require('./server');
+  } catch (err) {
+    console.error('Failed to start in-memory MongoDB:', err);
+    process.exit(1);
+  }
+})();
